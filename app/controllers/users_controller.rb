c6529e18df1e7f855fa1dce1105fd2cc4815b0df
Jason Wright <noobjey@gmail.com>
2015-07-28 21:10:52 -0600

    refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    flash[:new_user] = user.username
-    redirect_to songs_path
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to songs_path
+    end
   end
 
   def user_params
