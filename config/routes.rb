2adbe2d54a5545ecbcad2da12e4ee8a24aa67a93
Jason Wright <noobjey@gmail.com>
2015-07-29 08:13:25 -0600

    user can logout

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- spec/features/user_logins_spec.rb
+++ spec/features/user_logins_spec.rb
@@ -1,29 +1,22 @@
 require 'rails_helper'
 
-
-
 describe 'User' do
-
-
   it 'who is registered can login' do
     user = User.create(username: 'Joe', password: 'dirt')
     visit songs_path
 
-    expect(page).to_not have_content('Welcome noobjey')
+    expect(page).to_not have_content("Welcome #{user.username}")
 
     expect(page).to have_link('Login')
 
     click_link('Login')
 
-    expect(page).to have_field(user.username)
-    expect(page).to have_field('password')
-
-    fill_in 'session_username', with: 'noobjey'
-    fill_in 'session_password', with: 'password'
+    fill_in 'session_username', with: user.username
+    fill_in 'session_password', with: user.password
 
     click_button 'Login'
 
-    expect(page).to have_content('Welcome noobjey')
+    expect(page).to have_content("Welcome #{user.username}")
   end
 
 end
